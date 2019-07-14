d67219d8652e80a67129d5b325de26b6b2980581
Erik Nikulski <erik@nikulski.me>
2019-07-14 20:32:07 +0200

    Add initial state test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,9 +1,18 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility that Flutter provides. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:never_have_i_ever/env.dart';
+import 'package:never_have_i_ever/screens/app.dart';
 
 void main() {
+  testWidgets('Initial state', (WidgetTester tester) async {
+    BuildEnvironment.init(
+        flavor: BuildFlavor.development, baseUrl: 'https://api.neverhaveiever.io/v1/');
+    assert(env != null);
+
+    await tester.pumpWidget(App());
+
+    expect(find.text('Never Have I Ever'), findsOneWidget);
+    find.byWidget(CircularProgressIndicator());
+  });
 }
