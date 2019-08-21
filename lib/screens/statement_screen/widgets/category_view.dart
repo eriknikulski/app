5a1669f139066832c495cacd10553bee091d42e4
Erik Nikulski <erik@nikulski.me>
2019-08-21 15:01:09 +0200

    Remove unused import

--- lib/screens/statement_screen/widgets/category_view.dart
+++ lib/screens/statement_screen/widgets/category_view.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
-import 'package:flutter_svg/flutter_svg.dart';
-
 import 'package:never_have_i_ever/models/category_icon.dart';
 
 class CategoryView extends StatefulWidget {
